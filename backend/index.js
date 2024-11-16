4104269b35bcf96e5b2ec695ea265da6a36de651
Nicole Hernandez <hercanick@gmail.com>
2024-11-16 22:08:13 +0000

    Fix call to env file

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
-require('dotenv').config({ path: process.env.ENV_PATH });
+require('dotenv').config();
+console.log("PORT:", process.env.PORT);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("CORS_ORIGIN:", process.env.CORS_ORIGIN);
 const express = require('express');
 const app = express();
 const sequelize = require('./src/config/database');
